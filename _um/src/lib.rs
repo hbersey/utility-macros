581899e88c27dc12c450183cd3e76018b6061888
Henry Bersey <henry@bersey.com>
2024-05-09 15:11:45 +0100

    removed unused _um::path_

--- _um/src/lib.rs
+++ _um/src/lib.rs
@@ -6,13 +6,6 @@ pub mod required;
 pub mod container_attributes;
 pub mod field_attributes;
 
-#[macro_export]
-macro_rules! path_ {
-    ($root:ident$(::$e:ident)*) => {
-        ::utility_macros::_utility_macros::$root$(::$e)*
-    };
-}
-
 pub mod option;
 
 pub mod _sa {
