7a717089245fdc49286adb1e4e76b65495be3894
Henry Bersey <henry@bersey.com>
2024-05-16 17:54:05 +0100

    added some docs

--- _um/src/pick.rs
+++ _um/src/pick.rs
@@ -1,11 +1,16 @@
 use crate::error::Result;
 
+/// A trait for types that have a pick representation.
 pub trait HasPick {
+    /// The pick representation of the type.
     type Pick;
 
+    /// Converts the type to its pick representation.
     fn pick(&self) -> Result<Self::Pick>;
 }
 
+/// A trait for pick representations of types.
 pub trait Pick {
+    /// The type that the pick representation is for.
     type Type;
 }
--- _um/src/record.rs
+++ _um/src/record.rs
@@ -1,26 +1,41 @@
 use crate::error::Result;
 use std::ops::{Index, IndexMut};
 
+/// A trait for types that have a record representation.
 pub trait HasRecord: Sized {
+    /// The record representation of the type.
     type Record;
 
+    /// Converts the type to its record representation.
     fn as_str(&self) -> &'static str;
+    /// Converts the type to its record representation.
     fn try_from_str(s: &str) -> Result<Self>;
 }
 
+/// A trait for record representations of types.
 pub trait Record: Index<Self::Keys> + IndexMut<Self::Keys> + Sized {
+    /// The type that the record representation is for.
     type Keys: HasRecord<Record = Self>;
+    /// The type that the record representation is for.
     type Type;
 
+    /// The number of fields in the record.
     const N: usize;
 
+    /// The keys of the record.
     fn keys() -> Vec<Self::Keys>;
 
+    /// The values of the record.
     fn values(&self) -> Vec<&Self::Type>;
+    /// The mutable values of the record.
     fn values_mut(&mut self) -> Vec<&mut Self::Type>;
 
+    /// The entries of the record.
     fn entries(&self) -> Vec<(Self::Keys, &Self::Type)>;
+    /// The mutable entries of the record.
+    /// Note. that the keys are not mutable.
     fn entires_mut(&mut self) -> Vec<(Self::Keys, &mut Self::Type)>;
 
+    /// Try convert a vector of entries to the record representation.
     fn try_from_entries(entries: Vec<(Self::Keys, Self::Type)>) -> Result<Self>;
 }
