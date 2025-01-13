a35224eb9072060b6f2842e5107cccc4d8023261
Henry Bersey <henry@bersey.com>
2025-01-13 23:17:33 +0000

    time to rewrite!

--- _um/src/union/mod.rs
+++
@@ -1,8 +0,0 @@
-mod static_str_union;
-pub use static_str_union::StaticStrUnion;
-
-mod union;
-pub use union::Union;
-
-mod union_impl;
-pub use union_impl::union_impl as derive;
--- examples/static_str_union.rs
+++
@@ -1,9 +0,0 @@
-use utility_macros::union;
-
-fn main() {}
-
-union! {
-    type Side = "top" | "bottom";
-    #[derive(Clone)]
-    type Axis = "x" | "y";
-}
